6f2bf78b4a9c0a171946088719215bbf6a731445
sooncheer <handy-sun@foxmail.com>
2025-02-22 21:33:45 +0800

    fwh version 2.1

--- zsh-config.zsh
+++ zsh-config.zsh
@@ -106,7 +106,7 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_so=$'\E[01;47;34m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;36m'
-export LESS=-R
+[[ -z "$LESS" ]] && export LESS=-R
 
 zmodload zsh/terminfo
 
