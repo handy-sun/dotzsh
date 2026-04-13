dae71e4a8b6b70c7f914338e11d316bbbdd083c3
sooncheer <handy-sun@foxmail.com>
2026-04-13 23:37:04 +0800

    refactor: reformat flake.nix for improved readability and consistency

--- flake.nix
+++ flake.nix
@@ -11,123 +11,135 @@
     };
   };
 
-  outputs = inputs @ { self, flake-parts, ... }:
-  flake-parts.lib.mkFlake { inherit inputs; } {
-    imports = [
-      inputs.devshell.flakeModule
-    ];
-
-    flake.homeManagerModules.default = { config, lib, pkgs, ... }:
-    let
-      cfg = config.programs.dotzsh;
-      ## usually is `~/.nix-profile/bin`, sometimes `/etc/profiles/per-user/$USER/bin`
-      userNixProfileBin = "${config.home.profileDirectory}/bin";
-      systemBin = "/run/current-system/sw/bin:/usr/local/bin:/usr/bin:/bin";
-      setPathScript = ''
-        export PATH="${userNixProfileBin}:${systemBin}:$PATH"
-        if [ -f /opt/homebrew/bin/brew ]; then
-          export PATH="/opt/homebrew/bin:$PATH"
-        fi
-      '';
-    in {
-      options.programs.dotzsh = {
-        enable = lib.mkEnableOption "execute cm-init shell";
-        enableZshIntegration = lib.mkEnableOption "init Content in .zshrc";
-        enableFishIntegration = lib.mkEnableOption "init Content in .fishrc";
-        enableFishPrompt = lib.mkEnableOption "set fish_prompt and fish_right_prompt";
-      };
-
-      config = lib.mkMerge [
-        (lib.mkIf cfg.enable {
-          home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.cm-init ];
-          ## Same to  `nix run .#cm-init` and `nix run .#fish-init`
-          home.activation.runMyShellInit = lib.hm.dag.entryAfter ["writeBoundary"] ''
-            ${setPathScript}
-            echo "--- Running dotzsh shell init ---"
-            # echo "### Current PATH:"
-            # echo "$PATH" | tr ':' '\n'
-            # echo "### Current PATH [end]"
-            ${self.packages.${pkgs.stdenv.hostPlatform.system}.cm-init}/bin/dotzsh-cm
-            ${self.packages.${pkgs.stdenv.hostPlatform.system}.fish-init}/bin/dotzsh-fish
-            echo "--- Done ---"
-          '';
-        })
-
-        (lib.mkIf cfg.enableZshIntegration {
-          programs.zsh.initContent = lib.mkOrder 1200 ''
-            # --- github:handy/dotzsh flake auto-sourced ---
-            source ${self}/zshrc
-          '';
-        })
-
-        (lib.mkIf cfg.enableFishIntegration {
-          programs.fish.shellInitLast = let
-            commonFish = pkgs.runCommand "dotzsh-common.fish" {} ''
-              ${setPathScript}
-              ${pkgs.bash}/bin/bash ${./common.fish.in} stdout > $out
-            '';
-          in ''
-            # --- github:handy/dotzsh flake auto-sourced ---
-            source ${commonFish}
-          '';
-        })
-
-        (lib.mkIf cfg.enableFishPrompt {
-          programs.fish.interactiveShellInit = builtins.readFile ./fish/prompt.fish;
-        })
+  outputs =
+    inputs@{ self, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devshell.flakeModule
       ];
-    };
 
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
+      flake.homeManagerModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          cfg = config.programs.dotzsh;
+          ## usually is `~/.nix-profile/bin`, sometimes `/etc/profiles/per-user/$USER/bin`
+          userNixProfileBin = "${config.home.profileDirectory}/bin";
+          systemBin = "/run/current-system/sw/bin:/usr/local/bin:/usr/bin:/bin";
+          setPathScript = ''
+            export PATH="${userNixProfileBin}:${systemBin}:$PATH"
+            if [ -f /opt/homebrew/bin/brew ]; then
+              export PATH="/opt/homebrew/bin:$PATH"
+            fi
+          '';
+        in
+        {
+          options.programs.dotzsh = {
+            enable = lib.mkEnableOption "execute cm-init shell";
+            enableZshIntegration = lib.mkEnableOption "init Content in .zshrc";
+            enableFishIntegration = lib.mkEnableOption "init Content in .fishrc";
+            enableFishPrompt = lib.mkEnableOption "set fish_prompt and fish_right_prompt";
+          };
 
-    perSystem = { pkgs, system, ... }: {
-      packages = {
-        cm-init = pkgs.writeShellApplication {
-          name = "dotzsh-cm";
-          runtimeInputs = [
-            pkgs.bash
-            pkgs.coreutils
+          config = lib.mkMerge [
+            (lib.mkIf cfg.enable {
+              home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.cm-init ];
+              ## Same to  `nix run .#cm-init` and `nix run .#fish-init`
+              home.activation.runMyShellInit = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+                ${setPathScript}
+                echo "--- Running dotzsh shell init ---"
+                # echo "### Current PATH:"
+                # echo "$PATH" | tr ':' '\n'
+                # echo "### Current PATH [end]"
+                ${self.packages.${pkgs.stdenv.hostPlatform.system}.cm-init}/bin/dotzsh-cm
+                ${self.packages.${pkgs.stdenv.hostPlatform.system}.fish-init}/bin/dotzsh-fish
+                echo "--- Done ---"
+              '';
+            })
+
+            (lib.mkIf cfg.enableZshIntegration {
+              programs.zsh.initContent = lib.mkOrder 1200 ''
+                # --- github:handy/dotzsh flake auto-sourced ---
+                source ${self}/zshrc
+              '';
+            })
+
+            (lib.mkIf cfg.enableFishIntegration {
+              programs.fish.shellInitLast =
+                let
+                  commonFish = pkgs.runCommand "dotzsh-common.fish" { } ''
+                    ${setPathScript}
+                    ${pkgs.bash}/bin/bash ${./common.fish.in} stdout > $out
+                  '';
+                in
+                ''
+                  # --- github:handy/dotzsh flake auto-sourced ---
+                  source ${commonFish}
+                '';
+            })
+
+            (lib.mkIf cfg.enableFishPrompt {
+              programs.fish.interactiveShellInit = builtins.readFile ./fish/prompt.fish;
+            })
           ];
-          text = ''
-            ${pkgs.bash}/bin/bash ${./common.sh.in} -1
-          '';
         };
-        fish-init = pkgs.writeShellApplication {
-          name = "dotzsh-fish";
-          runtimeInputs = [
-            pkgs.bash
-            pkgs.coreutils
-          ];
-          text = ''
-            ${pkgs.bash}/bin/bash ${./common.fish.in} -1
-          '';
-        };
-      };
 
-      devshells.default = {
-        commands = [
-          {
-            help = "no";
-            name = "cm-init";
-            package = self.packages.${system}.cm-init;
-          }
-          {
-            help = "no";
-            name = "fish-init";
-            package = self.packages.${system}.fish-init;
-          }
-        ];
-        devshell = {
-          motd = ''
-            🔨 Welcome to devshell{reset}
-          '';
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, system, ... }:
+        {
+          packages = {
+            cm-init = pkgs.writeShellApplication {
+              name = "dotzsh-cm";
+              runtimeInputs = [
+                pkgs.bash
+                pkgs.coreutils
+              ];
+              text = ''
+                ${pkgs.bash}/bin/bash ${./common.sh.in} -1
+              '';
+            };
+            fish-init = pkgs.writeShellApplication {
+              name = "dotzsh-fish";
+              runtimeInputs = [
+                pkgs.bash
+                pkgs.coreutils
+              ];
+              text = ''
+                ${pkgs.bash}/bin/bash ${./common.fish.in} -1
+              '';
+            };
+          };
+
+          devshells.default = {
+            commands = [
+              {
+                help = "no";
+                name = "cm-init";
+                package = self.packages.${system}.cm-init;
+              }
+              {
+                help = "no";
+                name = "fish-init";
+                package = self.packages.${system}.fish-init;
+              }
+            ];
+            devshell = {
+              motd = ''
+                🔨 Welcome to devshell{reset}
+              '';
+            };
+          };
         };
-      };
     };
-  };
 }
